38aa91bd48fcbf3f349bc4c736dc6bf9c118cfc5
Harish Kurma <hkurma0504@gmail.com>
2024-01-17 03:55:49 -0500

    Fix issue with theme mode toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import * as React from 'react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -18,10 +17,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="small">
+        <Button className="relative" variant="ghost" size="small">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
